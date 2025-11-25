2e832ada589328e60954882c7a0a47a6923d0a75
Javierpin-bej <francisco.pinargote@epn.edu.ec>
2025-11-25 08:20:07 -0500

    Add files via upload

---
+++ Recursividad.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+	int num;
+	for(int i=0;i<=4;i++){
+		cout << i << endl;
+		cout << i + (i+1) << endl;
+	}
+}
+
+
+/*
+void fibonacci(int num) {
+	
+}
+
+int main () {
+	fibonacci(5);
+	return 0;
+}
+*/
