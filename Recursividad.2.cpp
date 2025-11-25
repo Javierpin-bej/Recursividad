df9bdb30c45a2a60191351f01d3da98d2e34b9d5
Javierpin-bej <francisco.pinargote@epn.edu.ec>
2025-11-25 08:39:41 -0500

    Add files via upload

--- Recursividad.2.cpp
+++ Recursividad.2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
+/*
 int main () {
 	int num;
 	for(int i=0;i<=4;i++){
@@ -9,15 +9,21 @@ int main () {
 		cout << i + (i+1) << endl;
 	}
 }
+*/
 
-
-/*
-void fibonacci(int num) {
-	
+long fibonacci(long num) {
+	if (num == 0) return 0;
+	if (num == 1) return 1;
+	return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 int main () {
-	fibonacci(5);
+	int n;
+	cout << ("Ingrese cuantos valores quiere mostrar: ");
+	cin >> n;
+	cout << ("Serie Fibonacci");
+	for(int i=0;i<=n;i++){
+		cout << fibonacci(i) << " ";
+	}
 	return 0;
 }
-*/
